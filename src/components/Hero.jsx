d61a5ac92b8d5ec3c01cb2f06c622f92d238ff5a
moiz064 <moizr064@gmail.com>
2023-08-09 12:37:08 +0500

    visual: heading re-aligned

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <img src={grid} alt="grid"
                 className='self-center mt-10 lg:w-[1000px] md:w-[600px] w-[400px]'
             />
-            <div className='ml-[10px]'>
+            <div className='ml-[10px] sm:ml-[10%]'>
                 <h1 className='font-bold text-3xl mt-10 mb-4 font-poppins'>
                     Online Experiences
                 </h1>
